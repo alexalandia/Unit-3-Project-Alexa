1ff82f2a8920752fb73ded0b41afce4b2c9f69af
alexalandia <123668056+alexalandia@users.noreply.github.com>
2023-01-26 10:48:10 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let storypart2Yesofcourse  = document.querySelector(".option-one-screen");
+
+let storypart2Maybelater  = document.querySelector(".option-two-screen");
+
+let storypart3Yesofcourse  = document.querySelector(".option-one-end");
+
+let storypart3Maybelater  = document.querySelector(".option-two-end");
+
+let buttom1 = document.querySelector(".option-one");
+let buttom2 = document.querySelector(".option-two");
+
+
+
+
+// When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables! //
+
+//
+buttom1.onclick=function(){
+storypart2Yesofcourse.style.display="block" ;
+};
+
+//
+//
+buttom2.onclick=function(){
+storypart2Maybelater.style.display="block" ;
+
+};
+
+//
